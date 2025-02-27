229931107c61da952c2bbca0371940af650455cb
Ela <teodocio.am@gmail.com>
2025-02-26 21:41:20 -0600

    feat: alphabetized and missing_email view for Customer controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   get "customers/index"
+
+  resources :customers do
+    collection do
+      get "alphabetized"
+      get "missing_email"
+    end
+  end
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
