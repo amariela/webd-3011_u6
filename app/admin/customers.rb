578cf460c1c0d96fb3777dbccef8facb39efd698
Ela <teodocio.am@gmail.com>
2025-02-26 19:02:17 -0600

    feat: add ability to attach image to Customer

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,5 +1,16 @@
 ActiveAdmin.register Customer do
   # these attributes can be changed
-  permit_params :full_name, :phone_number, :email, :notes
+  permit_params :full_name, :phone_number, :email, :notes, :image
   # all CRUD actions are enabled by default
+
+  form do |f|
+    f.inputs do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email
+      f.input :notes
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 end
